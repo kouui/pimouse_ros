27051b045fd497e7f63a402c6708daeaf22a94d4
kouui <lelouchkako@gmail.com>
2019-01-18 02:59:22 +0000

    Added lightsensors

---
+++ scripts/buzzer3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import UInt16
+
+def write_freq(hz=0):
+    bfile = "/dev/rtbuzzer0"
+    try:
+        with open(bfile, 'w') as file:
+            file.write("{}\n".format(hz))
+    except IOError:
+        rospy.logerr("can't write to {}".format(bfile))
+
+
+def recv_buzzer(data):
+    write_freq(data.data)
+
+if __name__ == "__main__":
+    rospy.init_node("buzzer")
+    rospy.Subscriber("buzzer", UInt16, recv_buzzer)
+    rospy.spin()
---
+++ scripts/lightsensor1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import sys, rospy
+from pimouse_ros.msg import LightSensorValues
+
+
+if __name__ == "__main__":
+    rospy.init_node("lightsensors")
